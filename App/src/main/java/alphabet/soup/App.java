e7ac7eced4573bb72b49a78ec4133c0ccdbafee1
cf2141 <cf2141@tgndomains.com>
2020-02-07 15:44:47 -0500

    next, merge

--- App/src/main/java/alphabet/soup/App.java
+++ App/src/main/java/alphabet/soup/App.java
@@ -4,10 +4,8 @@ package alphabet.soup;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Soup, Alphabet Soup, mixing it up." );
     }
 }
